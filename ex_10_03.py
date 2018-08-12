da43192b20891b8aa514f9660eeea4be12727d91
Kasiphat <kasiphatjp@gmail.com>
2018-08-12 21:15:21 +0700

    exercise 10.3: letter frequency

---
+++ ex_10_03.py
@@ -0,0 +1,41 @@
+print("Exercise 10.3")
+
+#prints the letters from a-z in decreasing order of frequency
+
+fname = input("Enter file name: ")
+try:
+    fhand = open(fname)
+except:
+    print("Error, invalid file")
+    quit()
+
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+counts = dict()
+for line in fhand:
+    line = line.lower()
+    words = line.split()
+    for word in words:
+        for letter in word:
+            if letter in alphabets:
+                counts[letter] = counts.get(letter, 0) + 1
+            else:
+                continue
+
+ls = list()
+for letter, count in counts.items():
+    newtup = (count, letter)
+    ls.append(newtup)
+
+ls = sorted(ls, reverse = True)
+
+for count, letter in ls:
+    print(letter, count)
+
+# This makes sure there are 26 letters
+num = 0
+for count, letter in ls:
+    num = num + 1
+
+print(ls[:5])
+print(num)
