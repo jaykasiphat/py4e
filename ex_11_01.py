0e751c4a76fe68db765d3d0aa4d72e649e602d1c
Kasiphat <kasiphatjp@gmail.com>
2018-08-15 14:49:18 +0700

    Exercise 11.1: simulate grep

---
+++ ex_11_01.py
@@ -0,0 +1,20 @@
+import re
+'''
+    Write a simple program to simulate the operation of the grep command on Unix. Ask the user to enter a regular expression and count the number of lines that matched the regular expression.
+'''
+
+print("Assignment 11.1")
+
+fhand = open('mbox.txt')
+regex = input("Enter a regular expression: ")
+
+count = 0
+for line in fhand:
+    line = line.rstrip()
+    x = re.findall(regex, line)
+    if len(x) < 1:
+        continue
+    else:
+        count += 1
+
+print("mbox.txt had", count, "lines that matched", regex)
