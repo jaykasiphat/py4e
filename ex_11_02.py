9fe411b40223f59a3ec420cb68738964d2279764
Kasiphat <kasiphatjp@gmail.com>
2018-08-15 15:35:42 +0700

    Modified Exercise 11.2

--- ex_11_02.py
+++ ex_11_02.py
@@ -1,10 +1,11 @@
 import re
 '''
     Write a program to look for lines of the form `New Revision: 39772`.
-    Extract the number from each of the lines using a regular expression and the findall() method. Compute the average of the numbers and print out the average.
+    Extract the number from each of the lines using a regular expression and the findall() method.
+    Compute the average of the numbers and print out the average.
 '''
-print("Exercise 11.2")
 
+print("Exercise 11.2")
 fname = input("Enter file name: ")
 fhand = open(fname)
 
