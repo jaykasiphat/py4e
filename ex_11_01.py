a74a3e89e150a9c142766e76b03de42925ec34ed
Kasiphat <kasiphatjp@gmail.com>
2018-08-15 14:52:55 +0700

    Modified Exercise 11.1

--- ex_11_01.py
+++ ex_11_01.py
@@ -1,6 +1,8 @@
 import re
 '''
-    Write a simple program to simulate the operation of the grep command on Unix. Ask the user to enter a regular expression and count the number of lines that matched the regular expression.
+    Write a simple program to simulate the operation of the grep
+    command on Unix. Ask the user to enter a regular expression
+    and count the number of lines that matched the regular expression.
 '''
 
 print("Assignment 11.1")
